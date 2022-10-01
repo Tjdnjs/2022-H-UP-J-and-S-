9c62682e9d13a4048ac5c8246da3e4e7fa8b83b4
Tjdnjs <2seowon@naver.com>
2022-10-01 11:05:54 +0900

    [update] password check

--- static/js/check.js
+++ static/js/check.js
@@ -1,9 +1,19 @@
+// function is_userid(asValue) {
+//   var regExp = /^(?=.*[a-zA-Z])[-a-zA-Z0-9_.]{4,10}$/;
+//   return regExp.test(asValue);
+// }
+
+// function is_password(asValue) {
+//   var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,16}$/;
+//   return regExp.test(asValue);
+// }
+
 function check_e(){
-  const t = document.getElementById('email').value;
-  console.log(t);
+  const e = document.getElementById('email').value;
+  console.log(e);
   $.ajax({
     type: "POST",
-    url: `/user/checke/${t}`,
+    url: `/user/checke/${e}`,
     success: function (response) {
       if (response["exists"]) {
           $("#help-e").text("이미 존재하는 이메일입니다.").removeClass("is-safe").addClass("is-danger")
@@ -17,11 +27,11 @@ function check_e(){
 }
 
 function check_dup(){
-  const t = document.getElementById('id').value;
-  console.log(t);
+  const id = document.getElementById('id').value;
+  console.log(id);
   $.ajax({
     type: "POST",
-    url: `/user/checkDup/${t}`,
+    url: `/user/checkDup/${id}`,
     success: function (response) {
       if (response["exists"]) {
           $("#help-id").text("이미 존재하는 아이디입니다.").removeClass("is-safe").addClass("is-danger")
@@ -32,4 +42,16 @@ function check_dup(){
       $("#help-id").removeClass("is-loading")
   }
   })
+}
+
+function check_pw(){
+  const pw = document.getElementById('password').value;
+  let pw_check = document.getElementById('pw_check').value;
+  console.log(pw, pw_check)
+  if (pw !== pw_check){
+    $("#help-pw").text("비밀번호가 일치하지 않습니다").removeClass("is-danger").addClass("is-success")
+  }
+  else {
+    $("#help-pw").text("비밀번호가 일치합니다").removeClass("is-success").addClass("is-success")
+  }
 }
