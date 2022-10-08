d0a09ed31730f3683befc3c65e579ff0a482f968
Chaewon Lee <lcw8447@naver.com>
2022-10-09 03:57:24 +0900

    menubar accordion

--- static/js/menuslide.js
+++
@@ -1,8 +0,0 @@
-$('.menu1').click(function(){
-  $('.menu2').slideUp();
-  if ($(this).children('.menu2').is(':hidden')){
-     $(this).children('.menu2').slideDown();
-  } else{
-     $(this).children('.menu2').slideUp();
-  }
-});
